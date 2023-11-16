a684d27cda5cedd8a5b6a9eef2698fa205f3b863
SarahPyle <149534448+SarahPyle@users.noreply.github.com>
2023-11-16 11:23:46 -0600

    Update BallotLinkedList.h

--- BallotLinkedList.h
+++ BallotLinkedList.h
@@ -6,23 +6,23 @@ class BallotLinkedList
 {
 public:
 	BallotLinkedList();
-	BallotLinkedList(BallotNode &headcopy, BallotNode &tailcopy);
+	
 	~BallotLinkedList();
 
-	void RemoveFront();
-	void AddItemToFront(Ballot& ballot);
+	void RemoveFront(); //Removes the first item in the linked list
+	void AddItemToFront(Ballot& ballot); //adds a ballot to the front of the linked list
 	Ballot& ReturnFrontItem(); //Can be used to loop through list. Basically just look at the Front of ballot list then remove the front ballot until it returns NULL. This would delete the list so it is adviced to make a copy
 
-	void RemoveItem(Ballot& ballot);
+	void RemoveItem(Ballot& ballot); //removes a specific ballot in the linked list by inputing a ballot
 
-	Ballot& FindBallot(double ballotID);
+	Ballot& FindBallot(double ballotID); //Finds a ballot based on the ballotID
 
 
 
 	
-	BallotLinkedList Copy();
+	BallotLinkedList Copy(); //makes a duplicated of a linked list *Won't effect the original list when editing the copy
 	
-	bool Empty();
+	bool Empty(); //returns true or false depending on if the first item in the list is NULL
 
 
 private:
