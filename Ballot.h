b61894ee6e7fe75fb2eee02961fa77dd2e64c3ff
ReeshiGhosal2003 <149533805+ReeshiGhosal2003@users.noreply.github.com>
2023-11-16 10:59:22 -0600

    Update Ballot.h

--- Ballot.h
+++ Ballot.h
@@ -4,24 +4,24 @@ class Ballot
 public:
 	double BallotID;
 	double DistrictID;
-	double GovernorVote;
-	double SenatorVote;
-	double PresidentialVote;
+	double GovernorVote;//Stores CandidateID
+	double SenatorVote;// Stores CandiateID
+	double PresidentialVote; // Stores CandidateID
 	bool FilledIn;
 
-	Ballot();
+	Ballot();//Constructors
 	Ballot(double BallotID1, double DistrictID1, double GovernorVote1, double SenatorVote1, double PresidentialVote1, bool FilledIn1);
 	Ballot(Ballot &ballot);
-	~Ballot();
+	~Ballot();//Destructor
 
 	bool Equals(Ballot ballot);
 	double GetBallotID() { return BallotID; } // Fetches the BallotId
-	double GetDistict() { return DistrictID; }
-	double GetGovVoteCandidate() { return GovernorVote; }
-	double GetSenVoteCandidate() { return SenatorVote; }
-	double GetPresVoteCandidate() { return PresidentialVote; }
-	bool VerifyFilledIn() { return FilledIn; }
-	void Print();
+	double GetDistict() { return DistrictID; } //Fetches and returns DistrictID
+	double GetGovVoteCandidate() { return GovernorVote; } //Fetches and returns CandidateID
+	double GetSenVoteCandidate() { return SenatorVote; }//Fetches and returns CandidateID
+	double GetPresVoteCandidate() { return PresidentialVote; }//Fetches and returns CandidateID
+	bool VerifyFilledIn() { return FilledIn; }// Checks if Ballot is filled in.
+	void Print();//Prints the Ballot Information
 
 
 
